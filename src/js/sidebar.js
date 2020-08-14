7bc052738d10776af637ad423bbdb8820018a94f
mariya <mariya2095@yandex.ru>
2020-08-14 14:03:24 +0300

    v2.2.2

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,10 +1,12 @@
 var buttonOpenMenu = document.querySelector('.btn-round--menu');
 var modalLeft = document.querySelector('.sidebar');
 var buttonCloseMenu = document.querySelector('.btn-round--back');
-var menu = document.querySelector('.menu__nav');
-var heightBody = document.querySelector('.page__block')
+var heightBody = document.querySelector('.page__block');
+var sidebarBody = document.querySelector(".sidebar__body");
+
 
 buttonOpenMenu.addEventListener('click',function() {
+    modalLeft.style.width = '100%';
     heightBody.classList.add('body-height-hidden');
     modalLeft.classList.remove('sidebar-hide');
     modalLeft.classList.add('sidebar-open');
@@ -14,12 +16,13 @@ buttonCloseMenu.onclick = function() {
     heightBody.classList.remove('body-height-hidden');
     modalLeft.classList.add('sidebar-hide');
     modalLeft.classList.remove('sidebar-open');
+    modalLeft.style.width = 'auto';
 }
-modalLeft.addEventListener('click', function (e) {
-    if (!menu.contains(e.target) && !buttonCloseMenu.contains(e.target)) {
-        heightBody.classList.remove('body-height-hidden');
-        modalLeft.classList.add('sidebar-hide');
-        modalLeft.classList.remove('sidebar-open');
-        console.log('modal-left');
+modalLeft.addEventListener("click", function (e) {
+    if (!sidebarBody.contains(e.target) && !buttonCloseMenu.contains(e.target)) {
+      heightBody.classList.remove("body-height-hidden");
+      modalLeft.classList.add("sidebar-hide");
+      modalLeft.classList.remove("sidebar-open");
+      console.log("modal-left");
     }
-});
+  });
